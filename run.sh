32f6dd5050bbec7875ed8afd81f5a6349a34d5ca
thoverik <tho.pham@veriksystems.com>
2020-11-19 10:31:00 +0700

    update k6

--- run.sh
+++ run.sh
@@ -2,12 +2,15 @@ read CLIENT
 echo monitor benchmark client $CLIENT
 WORKER=nginx
 BENCHMARK_CONTAINER=benchmark-client
+BENCHMARK_BASE=benchmark-base
 
 docker ps -a --filter name=$WORKER -aq | xargs -r docker rm -f
 docker ps -a --filter name=$BENCHMARK_CONTAINER -aq | xargs -r docker rm -f
+docker ps -a --filter name=$BENCHMARK_BASE -aq | xargs -r docker rm -f
 
 cd $CLIENT && docker-compose --compatibility up -d --b
 
 
 echo streaming resource from benchmark client  $CLIENT  ....
-docker stats $BENCHMARK_CONTAINER > $CLIENT.top 
+docker stats  > $CLIENT.top 
+# docker logs -f benchmark-client
