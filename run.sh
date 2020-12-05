82370ff85fd850383b02eeaa4a58e72a9a309dd7
root <root@ip-172-31-33-232.ap-southeast-2.compute.internal>
2020-12-05 14:38:32 +0000

    update k6 run

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ WORKER=nginx
 BENCHMARK_CONTAINER=benchmark-client
 BENCHMARK_BASE=benchmark-base
 
+# put current date as yyyy-mm-dd HH:MM:SS in $date
+run=$(date '+%Y-%m-%d-%H:%M:%S')
+
 docker ps -a --filter name=$WORKER -aq | xargs -r docker rm -f
 docker ps -a --filter name=$BENCHMARK_CONTAINER -aq | xargs -r docker rm -f
 docker ps -a --filter name=$BENCHMARK_BASE -aq | xargs -r docker rm -f
@@ -13,5 +16,5 @@ cd $CLIENT && docker-compose --compatibility up -d --b
 
 
 echo streaming resource from benchmark client  $CLIENT  ....
-docker stats  > $CLIENT.top 
+docker stats  > $CLIENT_$run.top 
 docker logs -f benchmark-client
